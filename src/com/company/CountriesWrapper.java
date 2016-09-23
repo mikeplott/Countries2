6a621550088b1895bb1242c89b887b14db48f968
michaelplott <mike.plott80@gmail.com>
2016-09-22 20:05:58 -0400

    parsed text file into a hashmap correctly. working on a save method.

---
+++ src/com/company/CountriesWrapper.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+
+/**
+ * Created by michaelplott on 9/22/16.
+ */
+public class CountriesWrapper {
+    public static ArrayList<Country> allCountries = new ArrayList<>();
+
+    public CountriesWrapper(ArrayList<Country> allCountries) {
+        this.allCountries = allCountries;
+    }
+
+    public ArrayList<Country> getAllCountries() {
+        return allCountries;
+    }
+}
---
+++ src/com/company/Country.java
@@ -0,0 +1,30 @@
+package com.company;
+
+/**
+ * Created by michaelplott on 9/22/16.
+ */
+public class Country {
+    String abbreviation;
+    String countryName;
+
+    public Country(String abbreviation, String countryName) {
+        this.abbreviation = abbreviation;
+        this.countryName = countryName;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "abbreviation='" + abbreviation + '\'' +
+                ", countryName='" + countryName + '\'' +
+                '}';
+    }
+}
